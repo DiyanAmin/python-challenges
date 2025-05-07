f3b9bb7c6f5b7c83d6dc33165de25b6074662da8
DiyanAmin <pakuptl3112@gmail.com>
2025-05-07 17:49:56 +0530

    Co-authored-by: Sumaya <samvb007@users.noreply.github.com>

--- operatorsprecedence.py
+++ operatorsprecedence.py
@@ -1,4 +1,16 @@
 #Operators Precedence
-userinput=int(input('Enter numbers: '))
-math=userinput//87(907+236)*6812**948-128+42
-print('Answer:\n',math)
+a=9
+b=3
+c=6
+d=5
+e=1
+expression=e%a**(c+b)/d #9**9  5,20,86,53,241%1
+print('Expression Answer:\n',expression)
+
+#2nd challenge
+name = "name"
+age = 13
+if name == "alex" and name == "diyan" or age <= 2:
+    print("Hello")
+else:
+    print("GoodBye")
