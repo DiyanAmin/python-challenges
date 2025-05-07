1e019e225a6cd973f34e964235d52c15df79eee0
DiyanAmin <pakuptl3112@gmail.com>
2025-05-07 18:19:52 +0530

    1commit

---
+++ meanvalue.py
@@ -0,0 +1,12 @@
+#The mean of 40 numbers is 38. Later on, I detected that I misread the number 56 as 36. Find the correct mean of given numbers
+total_number=40
+meanof40=38
+wrong_value=36
+correct_value=56
+#Finding mean value of 40 numbers
+sum=total_number*meanof40
+print('Sum of 40 numbers:\n',sum)
+#Correct Sum of Numbers
+randomname=sum-((wrong_value)-(correct_value))
+correct_mean=randomname/total_number
+print('Correct mean is:\n',correct_mean)
