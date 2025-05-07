15ffc46e3235759ae4d7ef074b7bef6fc7c65a27
DiyanAmin <pakuptl3112@gmail.com>
2025-05-07 18:34:29 +0530

    commit1_for_cyclist_commit2_for_meanvalue

---
+++ cyclist.py
@@ -0,0 +1,10 @@
+#Average Speed
+cyclist1=str(input('Enter name: '))
+cyclist2=str(input('Enter name: '))
+cyclist3='I do not know my name'
+speed=10,230
+averagespeed=10230/3
+print('Speed Cyclists are going at:\n',averagespeed)
+print('Average Speed:\n',averagespeed)
+slowerguy='They are all at same speed'
+print(slowerguy)
